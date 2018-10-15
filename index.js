87d98fb93c4635bd958a47193fc408a0b4b0c8a2
vkputuu@gmail.com <vkputuu@gmail.com>
2018-10-15 19:53:44 +0530

    server-setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
+const config = require('./config/database');
+const path = require ('path');
+
+mongoose.Promise = global.Promise;
+MongoClient.connect(config.uri ,{ useNewUrlParser: true }, (err)=>{
+    if(err) {
+        console.log('Could Not Connect to DB', err);
+    }
+    else {
+        console.log('Connected To DB' + config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist'));
+
+
+app.get('*', (req, res) =>{
+     res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    
+  });
+  
+  app.listen(2000 ,()=>{
+      console.log('Listening on port 2000');
+  });
